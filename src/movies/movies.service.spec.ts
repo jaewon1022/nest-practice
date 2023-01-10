47826388614ff1351a61aa5191cc52257d8c080d
jaewon1022 <andyjaewon@naver.com>
2023-01-10 13:15:35 +0900

    #3.4 완

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -48,4 +48,73 @@ describe('MoviesService', () => {
       }
     });
   });
+
+  describe('deleteOne()', () => {
+    it('deletes a movie', () => {
+      service.create({
+        title: 'testMovie',
+        genres: ['test'],
+        year: 2022,
+      });
+
+      const beforeDelete = service.getAll().length;
+
+      service.deleteOne(1);
+
+      const afterDelete = service.getAll().length;
+
+      expect(afterDelete).toBeLessThan(beforeDelete);
+    });
+
+    it('show throw 404 error', () => {
+      try {
+        service.deleteOne(999);
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+        expect(error.message).toEqual('Movie with ID 999 not found.');
+      }
+    });
+
+    describe('create()', () => {
+      it('should create movie', () => {
+        const beforeCreate = service.getAll().length;
+
+        service.create({
+          title: 'testMovie',
+          genres: ['test'],
+          year: 2022,
+        });
+
+        const afterCreate = service.getAll().length;
+
+        expect(afterCreate).toBeGreaterThan(beforeCreate);
+        console.log(beforeCreate, afterCreate);
+      });
+    });
+
+    describe('update()', () => {
+      it('should update movie', () => {
+        service.create({
+          title: 'testMovie',
+          genres: ['test'],
+          year: 2022,
+        });
+
+        service.update(1, {
+          title: 'updatedMovie',
+        });
+
+        const movie = service.getOne(1);
+        expect(movie.title).toEqual('updatedMovie');
+      });
+      it('show throw a NotFoundException', () => {
+        try {
+          service.update(999, {});
+        } catch (error) {
+          expect(error).toBeInstanceOf(NotFoundException);
+          expect(error.message).toEqual('Movie with ID 999 not found.');
+        }
+      });
+    });
+  });
 });
